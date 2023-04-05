38f5d479bb441b3e73ba5e8c48f4e81ad8e9b1ee
humaidiar <humaidiar123@gmail.com>
2023-04-06 11:46:50 +1200

    home

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -1,5 +1,14 @@
 function Home() {
-  return <h2>Welcome!</h2>
+  return (
+    <div className="container">
+      <div className="header-block">
+        <img src="/images/banner.png" alt="banner" />
+        <p>We help reunite lost pets with their owners.</p>
+      </div>
+      <div className="list-lost"></div>
+      <div className="list-found"></div>
+    </div>
+  )
 }
 
 export default Home
