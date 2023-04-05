ea4079144c534806d676b3ab2e69b4ec56450e6f
Sacha Beaumier <sacha.beaumier@gmail.com>
2023-04-06 10:47:45 +1200

    working on lost forms: && gp origin main

--- client/components/LostAnimalForm.tsx
+++ client/components/LostAnimalForm.tsx
@@ -1,2 +1,19 @@
 import { ChangeEvent, FormEvent, useState } from 'react'
 import { useAppDispatch } from '../hooks/redux'
+import { LostAnimal } from '../../common/LostAnimal'
+import { useNavigate } from 'react-router-dom'
+
+function AddLostAnimalForm() {
+  const dispatch = useAppDispatch()
+  const navigate = useNavigate()
+
+  const [lostAnimal, setLostAnimal] = useState({} as LostAnimal)
+
+  const handleLostAnimalChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setLostAnimal({ ...lostAnimal, [event.target.id]: event.target.value })
+  }
+
+  const handleLostAnimalSubmit = (evt: FormEvent) => {
+    evt.preventDefault()
+  }
+}
