ad0386d8e30871946cc20065410af1452af25609
Emily <student@pop-os.localdomain>
2023-04-06 09:56:12 +1200

    Found table created

---
+++ server/db/migrations/20230405214226_found.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('found', (table) => {
+    table.increments('id').primaary()
+    table.string('species')
+    table.string('photo')
+    table.string('user_id')
+    table.string('user_name')
+    table.string('user_contact')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable('found')
+}
