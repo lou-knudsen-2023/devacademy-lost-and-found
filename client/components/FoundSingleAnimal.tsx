20f10160a96e86fedd3d64be0ad47899bfe6ab9b
Shadae <114277579+shadae-ellmers@users.noreply.github.com>
2023-04-06 16:23:27 +1200

    Auth stuff front end

--- client/components/FoundSingleAnimal.tsx
+++ client/components/FoundSingleAnimal.tsx
@@ -1,6 +1,8 @@
 import { FoundAnimal } from '../../common/FoundAnimal'
 import { fetchDeleteFound } from '../actions/FoundAnimals'
 import { useAppDispatch } from '../hooks'
+import { IfAuthenticated, IfNotAuthenticated } from './Authenticated'
+import { useAuth0 } from '@auth0/auth0-react'
 
 interface Props {
   foundProp: FoundAnimal
@@ -9,6 +11,7 @@ interface Props {
 export default function SingleFoundAnimal(props: Props) {
   const { id, species, photo, user_name, user_contact } = props.foundProp
   const dispatch = useAppDispatch()
+  const { loginWithRedirect, logout, user } = useAuth0()
 
   return (
     <div className="lost-container">
@@ -22,7 +25,17 @@ export default function SingleFoundAnimal(props: Props) {
           <p>{user_name}</p>
           <br></br>
           <h2>If you find our loved pet contact us :</h2>
-          <p>{user_contact}</p>
+          <IfAuthenticated>
+            <p>{user_contact}</p>
+          </IfAuthenticated>
+          <IfNotAuthenticated>
+            <button
+              className="button is-primary"
+              onClick={() => loginWithRedirect()}
+            >
+              Please log in to view details
+            </button>
+          </IfNotAuthenticated>
         </div>
         {/* <button onClick={() => dispatch(fetchDeleteFound(id))}>Delete</button> */}
       </div>
