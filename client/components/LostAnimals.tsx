f6d79dfe80a0b96baf1c869669d586fb2c76e913
Jennifer Harper <121775498+jennifer-harper@users.noreply.github.com>
2023-04-07 10:59:51 +1200

    blah

--- client/components/LostAnimals.tsx
+++ client/components/LostAnimals.tsx
@@ -8,6 +8,13 @@ export default function AllLostAnimals() {
   const dispatch = useAppDispatch()
   const lostanimals = useAppSelector((state) => state.lostReducer)
   // variable using Appselector to the lost animal reducer
+  const [selected, setSelected] = useState('all')
+
+  const options = [
+    { label: 'All animals', value: 'all' },
+    { label: 'Cat', value: 'cat' },
+    { label: 'Dog', value: 'dog' },
+  ]
 
   useEffect(() => {
     dispatch(fetchAllLost())
