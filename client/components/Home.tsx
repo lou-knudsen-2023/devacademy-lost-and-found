99120486afaebfcd501b50bf1c653789e1883848
humaidiar <humaidiar123@gmail.com>
2023-04-06 14:52:14 +1200

    fix server Lost Api

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -9,11 +9,9 @@ function Home() {
         <p>We help reunite lost pets with their owners.</p>
       </div>
       <div className="list-lost">
-        {/* <AllLostAnimals /> */}
+        <AllLostAnimals />
       </div>
-      <div className="list-found">
-        {/* <AllFoundAnimals /> */}
-      </div>
+      <div className="list-found">{/* <AllFoundAnimals /> */}</div>
     </div>
   )
 }
