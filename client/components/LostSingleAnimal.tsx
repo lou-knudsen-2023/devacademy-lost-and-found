b52c287f028f1ae6d9388b8b271cf5864e9eed4c
Shadae <114277579+shadae-ellmers@users.noreply.github.com>
2023-04-07 09:49:19 +1200

    linting done except lostanimals

--- client/components/LostSingleAnimal.tsx
+++ client/components/LostSingleAnimal.tsx
@@ -1,52 +1,47 @@
 import { LostAnimal } from '../../common/LostAnimal'
-// need delete action
-import { useAppDispatch } from '../hooks'
-
 interface Props {
   lostProp: LostAnimal
 }
 
 export default function SingleLostAnimal(props: Props) {
-  const { id, name, species, photo, user_name, user_contact } = props.lostProp
-  const dispatch = useAppDispatch()
+  const { name, species, photo, user_name, user_contact } = props.lostProp
 
   return (
     <div className="card">
-      <div className='card-image'>
+      <div className="card-image">
         <figure className="image is-4by3">
-          <img src={photo} alt={species}/>
-        </figure> 
+          <img src={photo} alt={species} />
+        </figure>
       </div>
 
       <div className="card-content">
         <div className="card-header">
-          <p className='card-header-title'>{name}</p>
+          <p className="card-header-title">{name}</p>
         </div>
-    
-          <div id="species-content">
-            <p className="title is-4">Species: </p>
-            <p>{species}</p>
-          </div>
 
-            <br></br>
+        <div id="species-content">
+          <p className="title is-4">Species: </p>
+          <p>{species}</p>
+        </div>
 
-            <div id="user-name-content">
-            <p className="title is-4">Owner: </p>
-            <p>{user_name}</p>
-          </div>
+        <br></br>
 
-            <br></br>
+        <div id="user-name-content">
+          <p className="title is-4">Owner: </p>
+          <p>{user_name}</p>
+        </div>
 
-          <div id="contact-details-content">
-            <p className="title is-4">If you find our loved pet contact us: </p>
-            <p>{user_contact}</p>
-          </div>
-          {/* if we want to include when the card was posted */}
-          {/* <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time> */}
+        <br></br>
 
+        <div id="contact-details-content">
+          <p className="title is-4">If you find our loved pet contact us: </p>
+          <p>{user_contact}</p>
+        </div>
+        {/* if we want to include when the card was posted */}
+        {/* <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time> */}
       </div>
       <div>
-        <button className='button is-dark'>Found</button>
+        <button className="button is-dark">Found</button>
       </div>
       {/* Could be using delete button when someone click found, therefore it will be excluded from the list */}
     </div>
