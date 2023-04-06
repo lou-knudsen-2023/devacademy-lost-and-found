477549cef4d02c82507c94467462d31839a1d09a
Shadae <shadaejadeellmers@gmail.com>
2023-04-06 16:02:12 +1200

    WIP home

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -1,17 +1,24 @@
 import AllFoundAnimals from './FoundAnimals'
 import AllLostAnimals from './LostAnimals'
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
     <div className="container">
       <div className="header-block">
-        <img src="/images/banner.png" alt="banner" />
+        {/* <img src="/images/banner.png" alt="banner" /> */}
         <p>We help reunite lost pets with their owners.</p>
       </div>
-
-      <div className="list-lost">{/* <AllLostAnimals /> */}</div>
-
-      <div className="list-found">{/* <AllFoundAnimals /> */}</div>
+      <div className="list-container">
+        <div className="list-lost">
+          {/* feel free to change to another clickable thing */}
+          <Link to="lost">See all lost animals</Link>
+        </div>
+        <div className="list-found">
+          {/* feel free to change to another clickable thing */}
+          <Link to="found">See all found animals</Link>
+        </div>
+      </div>
     </div>
   )
 }
