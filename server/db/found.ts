8224c3e34949f1f97a867c7374c4988e7d1bef58
Emily <student@pop-os.localdomain>
2023-04-07 10:41:17 +1200

    found.ts issues resolved

--- server/db/found.ts
+++ server/db/found.ts
@@ -6,21 +6,6 @@ export function getAllFound(db = connection) {
   return db('found').select()
 }
 
-//sends found Pet with matching ID into found table
-export async function makeFound(id: number, db = connection) {
-  try {
-    const foundPet = await db('found').select().where('id', id).first()
-    if (foundPet) {
-      await db('found').insert(foundPet)
-      await db('found').where('id', id).del()
-      return true //indicates pet was successfully moved
-    }
-    return false //indicates pet was not moved
-  } catch (error) {
-    throw 500
-  }
-}
-
 //creates a new found pet and returns their info
 export function createFound(foundObj: FoundAnimal, db = connection) {
   return db('found')
