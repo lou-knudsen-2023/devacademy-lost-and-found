8c4e9d108e167a7bfd108560b9beb07df3893254
lou-knudsen-2023 <121981156+lou-knudsen-2023@users.noreply.github.com>
2023-04-07 10:23:07 +1200

    some scss

--- client/components/LostAnimals.tsx
+++ client/components/LostAnimals.tsx
@@ -15,9 +15,11 @@ export default function AllLostAnimals() {
 
   return (
     <>
-    <h1>Help me, Im lost</h1>
+      <div className="card-header">
+        <p className='card-header-title'>Help me, Im lost</p>
+      </div>
     <section>
-      <div className="card-list-container">
+      <div>
       {lostanimals.map((data) => (
         <SingleLostAnimal lostProp={data} key={data.id} />
       ))}
